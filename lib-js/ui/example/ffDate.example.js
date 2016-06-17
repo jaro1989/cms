307d37060aba20aac61dc8bc846022c9bec5c316
kiselyou <v.a.kiselyov@gmail.com>
2016-06-17 23:30:59 +0300

    Lib-JS: FFDate  (update)

--- lib-js/ui/example/ffDate.example.js
+++ lib-js/ui/example/ffDate.example.js
@@ -13,12 +13,48 @@
         .setWidthBlock(6)
         .setWidthCaption(2)
         .setRequired()
-        //.setSize('sm')
         .appendHTML(APPEND_ELEMENT);
 
-    new ui.FFDate('01/01/2001', 'name-1.2', 'Cption 1.2')
+    new ui.FFDate('01/01/2001', 'name-2.1', 'Cption 2.1')
         .setWidthBlock(3)
-        //.setWidthCaption(2)
         .setRequired()
-        //.setSize('sm')
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-2.2', 'Cption 2.2')
+        .setWidthBlock(3)
+        .setRequired()
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-2.3', 'Cption 2.3')
+        .setWidthBlock(3)
+        .setRequired()
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-2.4', 'Cption 2.4')
+        .setWidthBlock(3)
+        .setRequired()
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-3.1', 'Cption 3.1')
+        .setWidthBlock(3)
+        .setWidthCaption(3)
+        .setRequired()
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-3.2', 'Cption 3.2')
+        .setWidthBlock(3)
+        .setWidthCaption(3)
+        .setRequired()
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-3.3', 'Cption 3.3')
+        .setWidthBlock(3)
+        .setWidthCaption(3)
+        .setRequired()
+        .appendHTML(APPEND_ELEMENT);
+
+    new ui.FFDate('01/01/2001', 'name-3.4', 'Cption 3.4')
+        .setWidthBlock(3)
+        .setWidthCaption(3)
+        .setRequired()
         .appendHTML(APPEND_ELEMENT);
