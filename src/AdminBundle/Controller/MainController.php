2d1b241187ed81f7ae02d88d8af9aeaa996e54ba
kiselyou <v.a.kiselyov@gmail.com>
2016-08-21 22:52:04 +0300

    Update list

---
+++ src/AdminBundle/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class MainController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+
+        return $this->render(
+            'AdminBundle:main:index.html.php',
+            [
+                'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
+            ]
+        );
+    }
+}
---
+++ src/AdminBundle/Resources/views/main/index.html.php
@@ -0,0 +1,12 @@
+<?php
+//echo 'ssssssssssssssssssssss';
+//var_dump($base_dir);
+$view->extend('base.html.php');
+//
+$view['slots']->set('title', 'Title');
+$view['slots']->start('body');
+
+
+echo '000000000000000000000000000000000';
+$view['slots']->stop();
+?>
