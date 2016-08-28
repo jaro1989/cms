facc25adb447db354b1e6dcd2a8ba9edd79b392e
kiselyou <v.a.kiselyov@gmail.com>
2016-08-28 10:51:39 +0300

    create object user

--- src/AdminBundle/Controller/UserController.php
+++ src/AdminBundle/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
@@ -19,20 +20,38 @@ class UserController extends Controller
 
     public function listAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery("
+            SELECT u.username,
+                   u.email,
+                   u.isActive
+              FROM AdminBundle:User u
+        ");
+
         return $this->render(
             'AdminBundle:user:list.html.twig',
             [
-                //'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
+                'data' =>  $query->getResult()
             ]
         );
     }
 
     public function trashAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery("
+            SELECT u.username,
+                   u.email,
+                   u.isActive
+              FROM AdminBundle:User u
+        ");
+
         return $this->render(
             'AdminBundle:user:trash.html.twig',
             [
-                //'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
+                'data' =>  $query->getResult()
             ]
         );
     }
